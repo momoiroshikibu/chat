26bc0b072b63cbe5d87af2361dceb3ab7e7bc715
YutoTannaka <tannaka@MacBook.local>
2016-07-04 22:49:58 +0900

    add main.go

---
+++ main.go
@@ -0,0 +1,26 @@
+package main
+
+import (
+	"log"
+	"net/http"
+)
+
+func main() {
+	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		w.Write([]byte(`
+      <html>
+        <head>
+          <title>チャット</title>
+        <head>
+        <body>
+          Hello, Chat!
+        </body>
+      </html>
+    `))
+	})
+
+  // start Web Server
+  if err := http.ListenAndServe(":8000", nil); err != nil {
+    log.Fatal("ListenAndServe:", err)
+  }
+}
