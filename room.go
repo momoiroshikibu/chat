eccf11231028ec59a403fca1355702e85647e2c3
YutoTannaka <tannaka@MacBook.local>
2016-07-09 14:47:54 +0900

    ヘルパー関数を追加

--- room.go
+++ room.go
@@ -19,6 +19,15 @@ type room struct {
 	clients map[*client]bool
 }
 
+func newRoom() *room {
+	return &room {
+		forward: make(chan []byte),
+		join: make(chan *client),
+		leave: make(chan *client),
+		clients: make(map[*client]bool),
+	}
+}
+
 const (
 	socketBufferSize  = 1024
 	messageBufferSize = 256
